63e664b95e4b74234b508782730ede1679e70285
Vlad_Perepelytsyn <vladperep58@gmail.com>
2024-12-03 15:01:53 +0200

    add file LR1_Perepelytsyn_108.cpp

---
+++ LR1_Perepelytsyn_108.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Hello, Vlad" << endl;
+	cout << "Hello, Влад" << endl;
+}
+
+
+
+
+
