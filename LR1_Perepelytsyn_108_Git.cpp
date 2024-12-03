a1b9a36eeac01194a08f105bc992d72c0a96c53b
Vlad_Perepelytsyn <vladperep58@gmail.com>
2024-12-03 15:11:34 +0200

    new comment

---
+++ LR1_Perepelytsyn_108_Git.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Hello, Vlad" << endl;
+	cout << "Hello, Влад" << endl;
+}
+//тест git
+//коментар 2
+
+
+
+
